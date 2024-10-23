a0d55e2472f653ea6bbbd4c7d1e21fe63fda57a1
Facundo Lanusse <falanusse@mail.austral.edu.ar>
2024-10-23 19:01:46 -0300

    agrego linea de prueba

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const ejs = require('ejs');
-
+//probando tema de pull si alguien cambia el main antes
 const app = express();
 const port = process.env.PORT || 3000;
 
